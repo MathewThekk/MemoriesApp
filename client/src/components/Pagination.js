14d6cf7de76f2549845c5b318954f593a2860d99
Mathew Thekkadayil <71290474+MathewThekk@users.noreply.github.com>
2023-03-20 16:23:21 +0100

    pagination finally fkin working

--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -1,33 +1,36 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Pagination, PaginationItem } from '@material-ui/lab';
+import { Pagination, PaginationItem } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 import { getPostsByPage } from '../actions/postsActions';
 import useStyles from './paginationStyles';
 
 const Paginate = ({ page }) => {
+  const { numberOfPages } = useSelector((state) => state.posts);
   const dispatch = useDispatch();
+
   const classes = useStyles();
 
   useEffect(() => {
-    console.log(2)
-
-  }, []);
+    if (page) {
+      dispatch(getPostsByPage(page));
+    }
+  }, [dispatch, page]);
 
   console.log(1);
   
   return (
     <Pagination
-      // classes={{ ul: classes.ul }}
-      count={1}
+      classes={{ ul: classes.ul }}
+      count={numberOfPages}
       page={Number(page) || 1}
       variant="outlined"
       color="primary"
-      // renderItem={(item) => (
-      //   <PaginationItem {...item} component={Link} to={`/posts?page=${item.page}`} />
-      // )}
+      renderItem={(item) => (
+        <PaginationItem {...item} component={Link} to={`/posts?page=${item.page}`} />
+      )}
     />
   );
 };
